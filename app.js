0fdcdeddaec94cbc3bc65df6f7f0cd2382f8cd57
AlanMcClenaghan <alandmcclenaghan@icloud.com>
2019-01-31 22:27:26 +0000

    Defined UI variables and created function to add new task.

---
+++ app.js
@@ -0,0 +1,48 @@
+// Define UI variables
+const form = document.querySelector("#task-form");
+const taskList = document.querySelector(".collection");
+const clearButton = document.querySelector(".clear-tasks");
+const filter = document.querySelector("#filter");
+const taskInput = document.querySelector("#task");
+
+// Function to load all event listeners
+loadAllEventListeners();
+
+function loadAllEventListeners() {
+  // Add task event
+  form.addEventListener('submit', addTask);
+}
+
+// Add Task
+function addTask(e) {
+
+  e.preventDefault();
+
+  if (taskInput.value === '') {
+    alert('Add a task');
+  } else {
+
+    // Create li element
+    const li = document.createElement('li');
+    // Add class
+    li.className = 'collection-item';
+    // Create text node and append to li
+    li.appendChild(document.createTextNode(taskInput.value))
+
+    // Create new link element
+    const link = document.createElement('a')
+    // Add class
+    link.className = 'delete-item secondary-content';
+    // Add icon html
+    link.innerHTML = '<i class="fa fa-remove"></i>'
+
+    // Append link to li
+    li.appendChild(link);
+
+    // Append li to ul
+    taskList.appendChild(li);
+
+    // Clear input
+    taskInput.value = '';
+  }
+}
